c39b1cb277fa4d79dc0a90f50618ca18b6cf6951
Krishan Gharu <kslg257@gmail.com>
2022-06-29 15:34:56 +0000

    updates

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,38 @@
 let game = {
-    currentGame: [],
-    playerMoves: [],
+    currentGame: [], // an array
+    playerMoves: [], // an array
     score: 0,
     // An array for the different choices.
     choices: ["button1", "button2", "button3", "button4"]
 };
 
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+    showScore();
+    addTurn();
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+// to export objects into the test file.
 // we'll be exporting more than one object and function from this file,
 // so we need to put them in curly braces. 
-module.exports = { game };
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
+
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -3,9 +3,10 @@
  */
 
 // We'll call game game and then require game.js
-const { game } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
 
 // add a beforeAll function and we're going to load the index.html file  into Jests mock DOM.
+// beforeAll runs before all of the tests.
  beforeAll(() => {
     // Install the fs library, which  is part of node's default standard library. 
      let fs = require("fs");
@@ -35,3 +36,51 @@ const { game } = require("../game");
          expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
      });
  });
+
+describe("newGame works correctly", () => {
+    // beforeAll runs before all of the tests  
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    });
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should add one move to the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
+    });
+});
+
+describe("gameplay works correctly", () => {
+    // beforeEach runs before each test is run, so  we're going to reset the state each time.
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.currentMove = [];
+        addTurn();
+    });
+    // afterEach resets the state again after each test.
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+    test("should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain(game.currentGame[0] + "light");
+    });
+});
